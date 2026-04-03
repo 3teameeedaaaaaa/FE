5cbd2b8c3fd631c0275b9cacbbc74e54c17804a0
elvinleeys <herjun802@gmail.com>
2026-04-04 02:17:32 +0900

    feat: routing 처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import "./App.css";
 
+import { Route, Routes } from "react-router-dom";
+
+import MainLayout from "./layout/main-layout/MainLayout";
+import NoNavLayout from "./layout/no-nav-layout/NoNavLayout";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+
 function App() {
     return (
-        <>
-            <h1>Hi~</h1>
-        </>
+        <Routes>
+            <Route element={<MainLayout />}>
+                <Route path="/" element={<Home />} />
+            </Route>
+            <Route element={<NoNavLayout />}>
+                <Route path="/login" element={<Login />} />
+            </Route>
+        </Routes>
     );
 }
 
